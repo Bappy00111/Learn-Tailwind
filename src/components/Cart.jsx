edc882e0877d1d3e611f1cf17219c468460a57a9
Bappy Modok <bappymodok0011@gmail.com>
2024-09-14 19:55:12 +0600

    dark mode added

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Cart = () => {
   return (
-    <div className='h-screen flex justify-center items-center bg-gray-100 '>
+    <div className='h-screen flex justify-center items-center bg-gray-100 dark:bg-gray-950'>
       <div className='max-w-sm mx-auto p-8 bg-white rounded-xl shadow-md  space-y-2'>
         <img className='h-24 mx-auto rounded-full ring-4 ring-green-500 transform hover:scale-105 duration-500' src="https://img.freepik.com/premium-photo/default-male-user-icon-blank-profile-image-green-background-profile-picture-icon_962764-98397.jpg" alt="" />
         <div className='text-center space-y-2'>
